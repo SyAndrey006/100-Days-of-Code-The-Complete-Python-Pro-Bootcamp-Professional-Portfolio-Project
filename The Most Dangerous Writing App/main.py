9b12919f6e9c3bde961cec6d5e8155bf4db04e72
Andrii <synelnyk.andrey.1234567890@gmail.com>
2025-12-29 18:24:47 +0100

    created the most dangerous writing app for writers

---
+++ The Most Dangerous Writing App/main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import time
+
+root = tk.Tk()
+root.title("Dangerous writing")
+
+#window
+window_width = 800
+window_height = 450
+canvas = tk.Canvas(root, width=window_width, height=window_height, bg="white")
+canvas.pack()
+
+
+
+root.mainloop()
