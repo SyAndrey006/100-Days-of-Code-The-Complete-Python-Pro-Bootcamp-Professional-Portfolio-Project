ef6ae76cf9a0ad8e47275c4f633cc9a92ce5fedd
Andrii <synelnyk.andrey.1234567890@gmail.com>
2025-12-30 22:06:02 +0100

    for everything

--- Google Dinosaur Game automation/main.py
+++ Google Dinosaur Game automation/main.py
@@ -5,27 +5,71 @@ import time
 dino_x = 700
 dino_y = 260
 
+print("Program starts in 3 seconds, switch to window...")
 time.sleep(3)
 
 while True:
+    #for cactus
     img = ImageGrab.grab(
         bbox=(
-            dino_x + 110,
-            dino_y + 10,
-            dino_x + 130,
-            dino_y + 30
+            dino_x + 80,
+            dino_y + 5,
+            dino_x + 135,
+            dino_y + 33
         )
     )
+
+    bg_r, bg_g, bg_b = img.getpixel((0, 0))
+    is_obstacle_detected = False
 
     for x in range(img.width):
         for y in range(img.height):
             r, g, b = img.getpixel((x, y))
-            if r > 50:
+            diff = abs(r - bg_r) + abs(g - bg_g) + abs(b - bg_b)
+
+            if diff > 100:
                 pyautogui.press("space")
+                time.sleep(0.215 )
+                pyautogui.keyDown ("down")
+                time.sleep(0.1)
+                pyautogui.keyUp( "down")
+                is_obstacle_detected = True
                 break
-        else:
-            continue
-        break
+
+        if is_obstacle_detected:
+            break
+
+    if is_obstacle_detected:
+        time.sleep(0.01)
+        continue
+
+    #for flying dino
+    img = ImageGrab.grab(
+        bbox=(
+            dino_x + 80,
+            dino_y - 10,
+            dino_x + 140,
+            dino_y - 8
+        )
+    )
+
+    bg_r, bg_g, bg_b = img.getpixel((0, 0))
+    is_obstacle_detected = False
+
+    for x in range(img.width):
+        for y in range(img.height):
+            r, g, b = img.getpixel((x, y))
+            diff = abs(r - bg_r) + abs(g - bg_g) + abs(b - bg_b)
+
+            if diff > 100:
+                pyautogui.keyDown("down")
+                time.sleep(0.3)
+                pyautogui.keyUp("down")
+                is_obstacle_detected = True
+                break
+
+        if is_obstacle_detected:
+            break
 
     time.sleep(0.01)
 
